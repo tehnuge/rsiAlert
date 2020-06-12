fc86507b7e9725f69bc20d083d13b9df0fdef91c
John Duong <jhnduong@gmail.com>
2020-06-12 09:33:23 -0700

    add csv writing

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const av = require('alphavantage')
+const av = require('alphavantage');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-let stocks = ['msft']
+let stocks = ['msft', 'aapl']
+
+const writeToCsv = data => {
+  console.log("ROWS:", data)
+
+  const csvWriter = createCsvWriter({
+    path: 'rsiLogs.csv',
+    header: [
+      { id: 'date', title: 'DATE' },
+      { id: 'name', title: 'NAME' },
+      { id: 'rsi', title: 'RSI' }
+    ]
+  });
+
+  const records = [];
+  for (let i = 0; i < data.length; i++) {
+    let datum = data[i];
+    records.push({
+      date: datum.date,
+      name: datum.name,
+      rsi: datum.rsi
+    })
+  }
+
+  csvWriter.writeRecords(records)  
+    .then(() => {
+      console.log('...Done writing csv');
+    });
+}
 
 const dateToday = () => {
   let date = new Date();
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
-  let day = date.getDate() - 2;
+  let day = date.getDate() - 1;
 
   if (day < 10) {
     day = '0' + day;
@@ -23,16 +53,29 @@ const dateToday = () => {
 const main = (stocks, avKey) => {
 
   let a = av({ key: avKey })
+  // a.performance.sector().then(data => {
+  //   console.log(data)
+  // });
 
+  let rows = [];
+  let promises = [];
   stocks.forEach(stock => {
-    a.technical.rsi(stock, 'daily', 14, 'close').then(data => {
-      let days = data["Technical Analysis: RSI"];
-      let today = dateToday();
-      console.log('date: ', today)
-      let rsi = days[today]['RSI']
-      console.log(rsi)
+    promises.push(
+      a.technical.rsi(stock, 'daily', 14, 'close').then(data => {
+        let days = data["Technical Analysis: RSI"];
+        let today = dateToday();
+        let rsi = days[today]['RSI']
+        rows.push({
+          date: today,
+          name: stock,
+          rsi: rsi,
+        });
+      })
+    );
+  });
 
-    });
+  Promise.all(promises).then( values => {
+    writeToCsv(rows);
   });
 
 }
