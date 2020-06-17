7b3f2f9d4cff4ce29401b2b3ae4cf39b6728eb56
John Duong <jhnduong@gmail.com>
2020-06-17 13:38:28 -0700

    add timeout and promise handling

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const av = require('alphavantage');
+const {stocks} = require('./stocks');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const util = require('util');
+fs = require('fs');
 
+const avApiLim = 5;
+const TIMEOUT = 300000;
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-let stocks = ['msft', 'aapl']
 
 const writeToCsv = data => {
   console.log("ROWS:", data)
@@ -15,7 +19,8 @@ const writeToCsv = data => {
       { id: 'date', title: 'DATE' },
       { id: 'name', title: 'NAME' },
       { id: 'rsi', title: 'RSI' }
-    ]
+    ],
+    append: true
   });
 
   const records = [];
@@ -50,34 +55,80 @@ const dateToday = () => {
   return formattedDate = year + '-' + month + '-' + day
 }
 
-const main = (stocks, avKey) => {
-
-  let a = av({ key: avKey })
-  // a.performance.sector().then(data => {
-  //   console.log(data)
-  // });
-
-  let rows = [];
+const resolvePromise = stocks => {
   let promises = [];
   stocks.forEach(stock => {
-    promises.push(
-      a.technical.rsi(stock, 'daily', 14, 'close').then(data => {
-        let days = data["Technical Analysis: RSI"];
-        let today = dateToday();
-        let rsi = days[today]['RSI']
-        rows.push({
-          date: today,
-          name: stock,
-          rsi: rsi,
-        });
-      })
-    );
-  });
-
-  Promise.all(promises).then( values => {
+    promises.push(callAv(stock))
+  }
+  )
+  Promise.all(promises).then(() => {
     writeToCsv(rows);
-  });
-
+    rows = [];
+  }).catch(err => { console.log("ERR: ", err)})
 }
 
-main(stocks, process.env.KEY)
+const resolvePromises = allStocks => {
+  if(allStocks.length === 0){
+    return;
+  }
+  setTimeout(() => {
+    let temp = allStocks.pop();
+    let promises = [];
+    temp.forEach(stock => {
+      promises.push(callAv(stock))
+    })
+    Promise.all(promises).then(() => {
+      writeToCsv(rows);
+      rows = []
+      resolvePromises(allStocks);
+    }).catch(err => { console.log("ERR! ", err)})
+  }, TIMEOUT);
+}
+
+const callAv = stock => (
+  a.technical.rsi(stock, 'daily', 14, 'close').then(data => {
+    if (!data){
+      console.log('no data for ', stock);
+      return;
+    }
+    let days = data["Technical Analysis: RSI"];
+    let today = dateToday();
+    let rsi = days[today]['RSI']
+    rows.push({
+      date: today,
+      name: data["Meta Data"]["1: Symbol"],
+      rsi: rsi,
+    });
+  }).catch(err => {console.log("ERRORRR on :",data["Meta Data"]["1: Symbol"], err)})
+)
+
+let rows = [];
+let a = av({ key: process.env.KEY })
+
+const main = (stocks) => {
+  let allStocks = [];
+  let stocksChunk = [];
+  for(let i = 0; i<stocks.length; i++){
+    if(stocksChunk.length < avApiLim){
+      stock = stocks[i];
+      stocksChunk.push(stock);
+    }
+    else{
+      allStocks.push(stocksChunk);
+      stocksChunk = [];
+    }
+  }
+
+  if(stocksChunk.length > 0){
+    allStocks.push(stocksChunk)
+  }
+  let firstStocks = allStocks.pop();
+  resolvePromise(firstStocks);
+  resolvePromises(allStocks);
+}
+
+main(stocks)
+// let a = av({ key: process.env.KEY })
+//   a.performance.sector().then(data => {
+//     console.log(data)
+//   });
